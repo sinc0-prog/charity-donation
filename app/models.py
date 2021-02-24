4758ec1592471c79371c9fcfd09b0cf3dd42df23
Dawid <dawidkurekkrk@gmail.com>
2021-02-24 19:01:09 +0100

    Model Institution added

--- app/models.py
+++ app/models.py
@@ -2,3 +2,16 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length = 64)
+
+INSTITUTION = (
+    ('fundacja', 'fundacja'),
+    ('organizacja pozarządowa', 'organizacja pozarządowa'),
+    ('zbiórka lokalna', 'zbiórka lokalna'),
+)
+
+
+class Institution(models.Model):
+    name = models.Charfield(max_length = 128)
+    description = models.CharField(max_lenght = 255)
+    type_of = models.CharField(choices = INSTITUTION, default = 'fundacja')
+    categories = models.ManyToManyField(Category)
