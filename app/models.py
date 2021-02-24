c2c8248d00a80d3d2bce43173882c3983f4f7804
Dawid <dawidkurekkrk@gmail.com>
2021-02-24 19:53:19 +0100

    Model Donation  addeded & migrations

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length = 64)
@@ -15,3 +16,16 @@ class Institution(models.Model):
     description = models.CharField(max_length = 255)
     type_of = models.CharField(max_length = 64, choices = INSTITUTION, default = 'fundacja')
     categories = models.ManyToManyField(Category)
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category) 
+    institution = models.ForeignKey(Institution, on_delete=models.SET_NULL, null = True)
+    address =  models.CharField(max_length = 255)
+    phone_number = models.CharField(max_length = 20)
+    city = models.CharField(max_length = 64)
+    zip_code = models.CharField(max_length = 20)
+    pick_up_date = models.DateTimeField() 
+    pick_up_time = models.DateTimeField() 
+    pick_up_comment = models.CharField(max_length = 255)
+    user = models.ForeignKey(User, default= None, null=True, on_delete=models.SET_NULL)
